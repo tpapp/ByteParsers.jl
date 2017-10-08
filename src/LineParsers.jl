60d75bc74ec70300a9dc8f18121af5730c230fd7
Tamas K. Papp <tkpapp@gmail.com>
2017-10-08 16:58:19 +0200

    Rewrite with saner syntax, type wrappers, etc.

--- src/LineParsers.jl
+++ src/LineParsers.jl
@@ -1,299 +1,173 @@
 module LineParsers
 
+using MacroTools
 using ArgCheck
-using Parameters
 
 export
-    ByteVector,
-    MaybeParsed,
-    isparsed,
-    INVALID, EOL, EMPTY,
-    parsefield,
+    # generic
     parsedtype,
-    FixedWidth,
+    parsenext,
+    MaybeParsed,
+    # parsers
+    PositiveInteger,
+    PositiveFixedInteger,
     DateYYYYMMDD,
-    SkipField,
-    ViewField,
-    parseline
+    Line
 
 ######################################################################
-# general interface
-######################################################################
-struct ParserException <: Exception end
+# generic interface
 
-const ByteVector = Vector{UInt8}
+"A parser that parses to `MaybeParsed{T}` values."
+abstract type AbstractParser{T} end
 
-struct MaybeParsed{T,S <: Signed}
-    pos::S
+"""
+    parsedtype(parser)
+
+Return type of a parser, see [`parsenext`](@ref).
+"""
+function parsedtype end
+
+"""
+    parsenext(parser, str, pos, sep)::MaybeParsed{parsedtype(parser)}
+
+Parse `str` starting at `pos` using `parser`. Variable-length fields are parsed
+until terminated by `sep`. Return a [`MaybeParsed`](@ref) object.
+"""
+function parsenext end
+
+"""
+    MaybeParsed(pos, [value])
+
+When `pos > 0`, it is the position of the character after parsing, and `value`
+holds the value. When `pos < 0`, it wraps the error code.
+"""
+struct MaybeParsed{T}
+    pos::Int
     value::T
-    MaybeParsed{T}(pos::S) where {T,S} = new{T,S}(pos)
-    MaybeParsed(pos::S, value::T) where {T,S} = new{T,S}(pos, value)
+    MaybeParsed{T}(pos) where {T} = new{T}(pos)
+    MaybeParsed{T}(pos, value::T) where {T} = new{T}(pos, value)
+    MaybeParsed(pos, value::T) where {T} = new{T}(pos, value)
 end
 
-function Base.isequal(x::MaybeParsed{Tx,Sx}, y::MaybeParsed{Ty,Sy}) where {Tx,Sx,Ty,Sy}
-    if isbits(Tx) && isbits(Ty)
-        (x.pos == y.pos) & ifelse(x.pos > 0, isequal(x.value, y.value), true)
-    else
-        x.pos == y.pos && (x.pos > 0 ? isequal(x.value, y.value) : true)
+@inline pos_to_error(pos, errorcode = 0) = -pos
+
+macro checkpos(ex, label = :error)
+    @capture ex (pos_, value_) = rhs_
+    quote
+        maybeparsed = $(esc(rhs))
+        $(esc(pos)) = maybeparsed.pos
+        if $(esc(pos)) < 0
+            @goto $label
+        else
+            $(esc(value)) = maybeparsed.value
+        end
     end
-end
-
-isparsed(pos::Int) = pos > 0
-
-isparsed(mp::MaybeParsed) = isparsed(mp.pos) > 0
-
-Base.unsafe_get(mp::MaybeParsed) = mp.value
-
-pos_value(mp::MaybeParsed) = mp.pos, mp.value
-
-MaybeParsed{T}(mp::MaybeParsed{T}) where {T} = mp
-
-"Invalid character."
-const INVALID = -1
-
-"End of line."
-const EOL = -2
-
-"Empty field."
-const EMPTY = -3
-
-"""
-    parsefield(str, start, parser, separator, [len = length(string)])
-
-Parse a field in `str::ByteVector`, starting at byte `start`, using `parser`,
-which determines the type of the result. `separator` is either a separator which
-delimits fields, or the number of characters to parse.
-
-Parsing stops at byte `len`, which defaults to the length of `str`.
-
-The result is returned as a `MaybeParsed{T}` object, with either position at the
-last character parsed (or an error code), and the value. `parser` determines
-`T`.
-"""
-function parsefield end
-
-function _safe_char2uint8(c::Char)
-    @argcheck Int(c) ≤ 0x80
-    UInt8(c)
-end
-
-parsefield(str::ByteVector, start, T, sep::Char, args...) =
-    parsefield(str, start, T, _safe_char2uint8(sep), args...)
-
-_value_type_parameter(::Type{MaybeParsed{T,S}}) where {T,S} = T
-_value_type_parameter(T::S) where {S <: MaybeParsed} =
-    _value_type_parameter(S)
-
-"""
-    parsedtype(str, start, parser, separator)
-
-Return a type `T` such that `parsefield` called with the same parameters returns
-a value of type `MaybeParsed{T,S}`.
-
-!!! note
-
-    Relies on type inference unless overridden.
-"""
-function parsedtype(str::Tstr, start::Tstart, ::Type{T}, sep::Tsep) where
-    {Tstr, Tstart, T, Tsep}
-    T_ = Base._return_type(parsefield, Tuple{Tstr, Tstart, Type{T}, Tsep})
-    _value_type_parameter(T_)
-end
-
-"""
-
-   parsefield(::Void, pos, parser, sep)
-
-Return `MaybeParsed{T}(pos)`, where `T` matcheds the type parameter of the value
-returned by the same call with a `ByteVector` as the first parameter.
-
-The function is useful for writing type stable code when parsing the parsed has
-given up on a set of fields.
-"""
-function parsefield(::Void, pos::P, ::Type{T}, sep::S) where {P, T, S}
-    MaybeParsed{_value_type_parameter(T_)}(pos)
 end
 
 ######################################################################
 # integer parsing
 ######################################################################
 
-"""
-    parsefield(str, start, ::Type{<: Integer}, separator, [len])
+struct PositiveInteger{T <: Integer} end
 
-Parse an integer of the given type until `separator`.
+parsedtype(::PositiveInteger{T}) where T = T
 
-!!! warning
+PositiveInteger(T::Type{<:Integer} = Int) = PositiveInteger{T}()
 
-    Does not check overflow; if you have too many digits, this can be a
-    problem. Make sure you use a wide enough type.
-"""
-function parsefield(str::ByteVector, start, ::Type{T}, sep::UInt8,
-                    len = length(str)) where {T <: Integer}
+function parsenext(parser::PositiveInteger{T}, str, start, sep::C) where {T, C}
     n = zero(T)
-    z = UInt8('0')
+    z = C('0')
     pos = start
+    len = length(str)
     @inbounds while pos ≤ len
         chr = str[pos]
-        chr == sep && return MaybeParsed(pos == start ? EMPTY : pos + 1, n)
+        if chr == sep
+            if pos == start
+                pos == pos_to_error(pos)
+            else
+                pos += 1
+            end
+            break
+        end
         maybe_digit = chr - z
         if 0 ≤ maybe_digit ≤ 9
             n = n*10 + maybe_digit
             pos += 1
         else
-            return MaybeParsed{Int}(INVALID)
-        end
-    end
-    MaybeParsed{Int}(EOL)
-end
-
-"""
-    FixedWidth(n)
-
-Stops parsing after reading the given number of characters.
-"""
-struct FixedWidth
-    width::Int
-end
-
-"""
-    parsefield(str, start, ::Type{<: Integer}, width::Int, [len])
-
-Parse an integer of the given type using `width` digits.
-
-!!! warning
-
-    Does not check overflow; if you have too many digits, this can be a problem.
-
-    Does not verify that any kind of separator follows the digits.
-"""
-function parsefield(str::ByteVector, start, ::Type{T}, width::FixedWidth,
-                    len = length(str)) where {T <: Integer}
-    n = 0
-    z = UInt8('0')
-    pos = start
-    stop = start + width.width
-    stop ≤ len + 1 || return MaybeParsed{Int}(EOL)
-    @inbounds while pos < stop
-        maybe_digit = str[pos] - z
-        if 0 ≤ maybe_digit ≤ 9
-            n = n*10 + maybe_digit
-            pos += 1
-        else
-            return MaybeParsed{Int}(INVALID)
+            pos = pos_to_error(pos)
+            break
         end
     end
     MaybeParsed(pos, n)
 end
 
-struct DateYYYYMMDD{strict} end
-
-"""
-    parsefield(str, start, ::Type{DateYYYYMMDD}, strict = true)
-
-Parse dates of the form "yyyymmdd".
-
-When `strict = false`, zero days are replaced by 1 (a peculiarity of the
-dataset).
-"""
-function parsefield(str::ByteVector, start, ::Type{DateYYYYMMDD{strict}},
-                    sep::UInt8, len = length(str)) where {strict}
-    stop = start+8
-    len ≥ stop || return MaybeParsed{Date}(EOL)
-
-    pos, y = pos_value(parsefield(str, start, Int, FixedWidth(4)))
-    isparsed(pos) || return MaybeParsed{Date}(pos)
-
-    pos, m = pos_value(parsefield(str, pos, Int, FixedWidth(2)))
-    isparsed(pos) || return MaybeParsed{Date}(pos)
-
-    pos, d = pos_value(parsefield(str, pos, Int, FixedWidth(2)))
-    isparsed(pos) || return MaybeParsed{Date}(pos)
-
-    str[pos] == sep || @goto invalid
-
-    d == 0 && (strict ? @goto(invalid) : (d = 1))
-
-    (m ≤ 12 && d ≤ Base.Dates.daysinmonth(y, m)) || @goto invalid
-
-    return MaybeParsed(pos + 1, Date(y, m, d))
-
-    @label invalid
-    MaybeParsed{Date}(INVALID)
-end
-
-"""
-    SkipField()
-
-Skip the field, returning `nothing`.
-"""
-struct SkipField end
-
-function parsefield(str::ByteVector, start, ::Type{SkipField}, sep::UInt8,
-                    len = length(str))
-    pos = start
-    @inbounds while pos ≤ len
-        str[pos] == sep && return MaybeParsed(pos + 1, nothing)
-        pos += 1
+struct PositiveFixedInteger{T <: Integer}
+    width::Int
+    function PositiveFixedInteger{T}(width::Int) where {T}
+        @argcheck width ≥ 1
+        new{T}(width)
     end
-    MaybeParsed{Void}(EOL)
 end
 
-"""
-    ViewField
+PositiveFixedInteger(width, T::Type{<: Integer} = Int) =
+    PositiveFixedInteger{T}(width)
 
-Return the contents of the field as a `SubArray` (view).
-"""
-struct ViewField end
-
-function parsefield(str::ByteVector, start, ::Type{ViewField}, sep::UInt8,
-                    len = length(str))
-    pos, _ = pos_value(parsefield(str, start, SkipField, sep, len))
-    MaybeParsed(pos, @view(str[start:(ifelse(isparsed(pos),pos-2,0))]))
-end
-
-######################################################################
-# line parsing
-######################################################################
-
-"""
-    parseline(str, sep, parsers...)
-
-Parse `str`, which contains fields separated by `sep`, using `parsers`.
-
-Return a `MaybeParsed(pos, values)`, where `values` is a tuple of parsed values
-if parsing was successful. Note that values for a `SkipField` parser are skipped.
-"""
-@generated function parseline(str, sep, parsers...)
-    extractT(::Type{MaybeParsed{T,S}}) where {T,S} = T
-    extractT(::Type{Type{T}}) where {T} = T
-    pos_var = :pos
-    field_vars = [Symbol(:field, i) for i in 1:length(parsers)]
-    invalid_label = :invalid
-    parser_blocks = Any[]
-    kept_field_vars = Symbol[]
-    parsed_types = Any[]
-    for (parser, field_var) in zip(parsers, field_vars)
-        parserT = extractT(parser)
-        push!(parser_blocks,
-              :($field_var = parsefield(str, $pos_var, $parserT, sep)),
-              :($pos_var = $field_var.pos),
-              :(isparsed($pos_var) || @goto $invalid_label))
-        if parserT ≠ SkipField
-            tt = Tuple{str, Int, parser, sep}
-            # NOTE manual says not to use Core.Inference in generated functions
-            push!(parsed_types, extractT(Base._return_type(parsefield, tt)))
-            push!(kept_field_vars, field_var)
+function parsenext(parser::PositiveFixedInteger{T}, str, start, sep::C) where {T, C}
+    n = zero(T)
+    z = C('0')
+    pos = start
+    stop = start + parser.width - 1
+    @assert stop ≤ length(str)
+    @inbounds while pos ≤ stop
+        chr = str[pos]
+        maybe_digit = chr - z
+        if 0 ≤ maybe_digit ≤ 9
+            n = n*10 + maybe_digit
+            pos += 1
+        else
+            pos = pos_to_error(pos)
+            break
         end
     end
-    parser_blocks_ex = quote $(parser_blocks...) end
-    field_vars_ex = :($(map(v -> :(unsafe_get($v)), kept_field_vars)...),)
+    MaybeParsed(pos, n)
+end
+
+struct DateYYYYMMDD end
+
+parsedtype(::DateYYYYMMDD) = Date
+
+function parsenext(parser::DateYYYYMMDD, str, pos, sep)
+    @checkpos (pos, year) = parsenext(PositiveFixedInteger(4), str, pos, sep)
+    @checkpos (pos, month) = parsenext(PositiveFixedInteger(2), str, pos, sep)
+    @checkpos (pos, day) = parsenext(PositiveFixedInteger(2), str, pos, sep)
+    str[pos] == sep || @goto error
+    pos += 1
+    return MaybeParsed(pos, Date(year, month, day))
+    @label error
+    MaybeParsed{Date}(pos)
+end
+
+######################################################################
+# lines
+######################################################################
+
+struct Line{T <: Tuple, S}
+    parsers::T
+end
+
+Line{T <: Tuple}(parsers::T) = Line{T, Tuple{map(parsedtype, parsers)...}}(parsers)
+
+const LineN{N, S} = Line{T, S} where T <: NTuple{N, Any}
+
+@generated function parsenext(line::LineN{N, S}, str, pos, sep) where {N, S}
     quote
-        $pos_var = 1
-        $(parser_blocks_ex)
-        return MaybeParsed($pos_var, $(field_vars_ex))
-        @label $invalid_label
-        MaybeParsed{Tuple{$(parsed_types)...}}($pos_var)
+        R = MaybeParsed{S}
+        Base.@nexprs $N j -> begin
+            @checkpos (pos, value_j) = parsenext(line.parsers[j], str, pos, sep)
+        end
+        return R(pos, Base.@ntuple $N value)
+        @label error
+        R(pos)
     end
 end
 
